c569bf5d427111b5f8c725b5ba27dbf4f0d29e45
David Bauer <david.bauer009@gmail.com>
2016-09-27 12:30:08 +0200

    Initial Commit

---
+++ addgif.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+
+import aqt
+from anki.hooks import addHook
+from aqt.utils import showInfo
+import urllib2
+import urllib
+from bs4 import BeautifulSoup
+from aqt.qt import *
+import re
+
+
+def cleanhtml(raw_html):
+  cleanr =re.compile('<.*?>')
+  cleantext = re.sub(cleanr,'', raw_html)
+  return cleantext
+
+def showText():
+    text = aqt.mw.reviewer.card.q()
+    showInfo("%s" % text)
+
+def mungeQA(html, type, fields, model, data, col):
+    try:
+        raw = cleanhtml(html)
+
+        if (type == 'q' and not len(raw) > 1):
+            url = 'http://www.sp.cis.iwate-u.ac.jp/icampus/u/akanji.jsp?k='
+            url += urllib.quote_plus(raw.encode('utf-8'))
+
+            source = urllib2.urlopen(url).read()
+            soup = BeautifulSoup(source, "html.parser")
+            imgs = soup.findAll("img")
+            filename = imgs[2]["src"].split("/")[-1]
+            html+="<br/><br/><span style='align:center'><img src='http://www.sp.cis.iwate-u.ac.jp/icampus/u/sod/"+filename+"' width='500px'></span>"
+            #showInfo("Munged: %s" % filename)
+    except:
+        print("Couldn't fetch stroke graph")
+
+    return html
+
+
+# create a new menu item, "test"
+action = QAction("test", aqt.mw)
+# set it to call testFunction when it's clicked
+action.triggered.connect(showText)
+# and add it to the tools menu
+aqt.mw.form.menuTools.addAction(action)
+
+addHook("mungeQA", mungeQA)
